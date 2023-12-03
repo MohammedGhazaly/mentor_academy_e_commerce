d4f2867c1701aaa77d9e900e138f152dabab8675
MohammedGhazaly <warendragon67@gmail.com>
2023-12-03 21:36:17 +0200

    Added image to our app and also finished the onboarding model

---
+++ lib/core/managers/images.dart
@@ -0,0 +1,5 @@
+const String baseImagePath = "assets/images/";
+
+class AppImages {
+  static const String onBoardingImage = "${baseImagePath}onboarding.png";
+}
---
+++ lib/core/managers/lists.dart
@@ -0,0 +1,20 @@
+import 'package:mentor_academy_e_commerce/core/managers/images.dart';
+import 'package:mentor_academy_e_commerce/models/onboarding_model.dart';
+
+class AppLists {
+  static List<OnBoradingModel> onBoradingModel = [
+    OnBoradingModel(
+        title: "Welcome",
+        subtitle: "We have many products.",
+        image: AppImages.onBoardingImage),
+    OnBoradingModel(
+        title: "Shopping made easy",
+        subtitle: "We guarantee you will have good time shopping.",
+        image: AppImages.onBoardingImage),
+    OnBoradingModel(
+      title: "Start now",
+      subtitle: "Start browsing through thousands of items.",
+      image: AppImages.onBoardingImage,
+    ),
+  ];
+}
