0712dc1743e4da9b2befab5431dc4951a2ca2a64
MohammedGhazaly <warendragon67@gmail.com>
2023-12-06 06:30:56 +0200

    Added constants for the api endpoint and created cubit

--- lib/core/network/api_constants.dart
+++ lib/core/network/api_constants.dart
@@ -2,4 +2,5 @@ class ApiConstants {
   static String baseUrl = "https://elwekala.onrender.com";
   static String registerEndPoint = "$baseUrl/user/register";
   static String loginEndPoint = "$baseUrl/user/login";
+  static String getProductsEndPoint = "$baseUrl/product/Laptops";
 }
