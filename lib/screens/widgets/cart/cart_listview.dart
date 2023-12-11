d2b3575872663ad685f76f2c45b8b0542efdaaf5
MohammedGhazaly <warendragon67@gmail.com>
2023-12-11 16:16:21 +0200

    Added animation for cart listview

--- lib/screens/widgets/cart/cart_listview.dart
+++ lib/screens/widgets/cart/cart_listview.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:mentor_academy_e_commerce/models/cart/cart_model/product.dart';
 import 'package:mentor_academy_e_commerce/screens/widgets/cart/cart_item.dart';
 
@@ -9,14 +10,23 @@ class CartListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        padding: EdgeInsets.symmetric(horizontal: 16.w),
-        itemCount: products.length,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: EdgeInsets.symmetric(vertical: 8.h),
-            child: CartItem(product: products[index]),
-          );
-        });
+    return AnimationLimiter(
+      child: ListView.builder(
+          padding: EdgeInsets.symmetric(horizontal: 16.w),
+          itemCount: products.length,
+          itemBuilder: (context, index) {
+            return AnimationConfiguration.staggeredList(
+              duration: Duration(milliseconds: 500),
+              position: index,
+              child: SlideAnimation(
+                horizontalOffset: -300.0,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8.h),
+                  child: CartItem(product: products[index]),
+                ),
+              ),
+            );
+          }),
+    );
   }
 }
